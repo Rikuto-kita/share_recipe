8de24fd70ac9c2a98c64a11bf88d6c2fc0d9b796
rikupoo <nemutir@gmail.com>
2020-10-15 01:24:03 +0900

    レシピ投稿機能

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,9 @@
 class Recipe < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :user
+  has_one_attached :image
+  belongs_to_active_hash :category
+
+  validates :image,:name, :foodstuff,:cook, :category, presence: true
+  validates :category_id, numericality: { other_than: 1 } 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  root to: 'recipes#index'
   devise_for :users
   resources :users, only: [:edit, :update,:delete,:show]
-  resources :recipes
+  resources :recipes #do
+    #collection do
+     #get 'search'
+    #end
+  #end
 end
